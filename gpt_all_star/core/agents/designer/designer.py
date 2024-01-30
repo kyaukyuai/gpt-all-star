7b60e404022329e18ed751656836fdb7d83efe87
kyaukyuai <y.kakui@gmail.com>
2024-01-30 20:51:34 +0900

    Implement todo list prompt for designer agent

--- gpt_all_star/core/agents/designer/designer.py
+++ gpt_all_star/core/agents/designer/designer.py
@@ -1,11 +1,11 @@
 from gpt_all_star.core.message import Message
 from gpt_all_star.core.storage import Storages
 from gpt_all_star.core.agents.agent import Agent, AgentRole
-from gpt_all_star.core.agents.designer.design_user_interface_prompt import (
-    design_user_interface_template,
-)
 from gpt_all_star.core.agents.designer.planning_ui_design_prompt import (
     planning_ui_design_template,
+)
+from gpt_all_star.core.agents.designer.implement_planning_prompt import (
+    implement_planning_prompt_template,
 )
 from gpt_all_star.core.steps import step_prompts
 from gpt_all_star.tool.text_parser import TextParser
@@ -84,24 +84,51 @@ class Designer(Agent):
         self.chat()
         self.console.new_lines(2)
 
-        # for file_name, file_str in self.storages.root.recursive_file_search().items():
-        #     self.console.print(
-        #         f"Adding file {file_name} to the prompt...", style="blue"
-        #     )
-        #     code_input = step_prompts.format_file_to_input(file_name, file_str)
-        #     self.messages.append(Message.create_system_message(f"{code_input}"))
-        # self.console.new_lines()
+        todo_list = TextParser.to_json(self.latest_message_content())
+        self.console.print(todo_list)
 
-        # self.messages.append(
-        #     Message.create_system_message(
-        #         design_user_interface_template.format(
-        #             specifications=self.storages.docs["specifications.md"]
-        #         )
-        #     )
-        # )
+        for i, task in enumerate(todo_list["plan"]):
+            self.console.print(f"TODO {i + 1}: {task['todo']}")
+            self.console.print(f"GOAL: {task['goal']}")
+            self.console.new_lines()
 
-        # self.chat()
+            current_contents = ""
+            for (
+                file_name,
+                file_str,
+            ) in self.storages.root.recursive_file_search().items():
+                self.console.print(
+                    f"Adding file {file_name} to the prompt...", style="blue"
+                )
+                code_input = step_prompts.format_file_to_input(file_name, file_str)
+                current_contents += f"{code_input}\n"
 
-        # files = TextParser.parse_code_from_text(self.latest_message_content())
-        # for file_name, file_content in files:
-        #     self.storages.root[file_name] = file_content
+            previous_finished_task_message = (
+                "All preceding tasks have been completed. No further action is required on them.\n"
+                + "All codes implemented so far are listed below. Please include them to ensure that we achieve our goal.\n"
+                + "{current_contents}\n\n"
+                if i == 0
+                else ""
+            )
+            self.messages.append(
+                Message.create_system_message(
+                    implement_planning_prompt_template.format(
+                        num_of_todo=len(todo_list["plan"]),
+                        todo_list="".join(
+                            [
+                                f"{i + 1}: {task['todo']}\n"
+                                for i, task in enumerate(todo_list["plan"])
+                            ]
+                        ),
+                        index_of_todo=i + 1,
+                        todo_description=task["todo"],
+                        finished_todo_message=previous_finished_task_message,
+                        todo_goal=task["goal"],
+                    )
+                )
+            )
+            self.chat()
+            self.console.new_lines(2)
+            files = TextParser.parse_code_from_text(self.latest_message_content())
+            for file_name, file_content in files:
+                self.storages.root[file_name] = file_content
---
+++ gpt_all_star/core/agents/designer/implement_planning_prompt.py
@@ -0,0 +1,51 @@
+from langchain_core.prompts import PromptTemplate
+
+implement_planning_prompt_template = PromptTemplate.from_template(
+    """We've broken down the plan into {num_of_todo} TODOs.
+```
+{todo_list}
+```
+
+You are currently working on TODO{index_of_todo} with the following description:
+```
+{todo_description}
+```
+
+{finished_todo_message}
+After a TODO is finished, please make sure you meet the goal: {todo_goal}.
+If you have already implemented a TODO that you need to do, exit without outputting anything.
+
+You will output the content of each file necessary to achieve the goal, including ALL code.
+**IMPORTANT**: Final answer must be the full codes, only the codes and nothing else.
+**IMPORTANT**: Never use placeholders!
+
+Follow best practice file naming conventions appropriate for your language and framework.
+Make sure files contain all imports, types, etc.
+Make sure code is fully functional.
+Ensure that code in different files is compatible.
+Include module dependencies or package manager dependency definition files.
+    For Python, you always create an appropriate requirements.txt file.
+    For NodeJS, you always create an appropriate package.json file.
+        please set NODE_OPTIONS=--openssl-legacy-provider like so:
+            "start": "NODE_OPTIONS=--openssl-legacy-provider react-scripts start"
+            "build": "NODE_OPTIONS=--openssl-legacy-provider react-scripts build"
+
+Represent files like so:
+
+FILENAME
+```
+CODE
+```
+
+The following tokens must be replaced like so:
+FILENAME is the lowercase combined path and file name including the file extension and **if the file already exists, please follow its name**.
+CODE is the code in the file
+
+Example representation of a file:
+
+./hello_world.py
+```
+print("Hello World")
+```
+"""
+)
