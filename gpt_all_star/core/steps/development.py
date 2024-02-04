029ad30021f1adfc63cf95a1b548bbed93a9b761
kyaukyuai <y.kakui@gmail.com>
2024-02-04 23:52:53 +0900

    Refactor Development step to use supervisor's data

--- gpt_all_star/core/steps/development.py
+++ gpt_all_star/core/steps/development.py
@@ -27,19 +27,22 @@ class Development(Step):
             auto_mode=self.auto_mode
         )
         for i, task in enumerate(todo_list["plan"]):
-            self.console.print(f"TODO {i + 1}: {task['todo']}")
-            self.console.print(f"GOAL: {task['goal']}")
-            self.console.print("---")
+            team.supervisor.state(
+                f"""TODO {i + 1}: {task['todo']})
+GOAL: {task['goal']}
+---
+"""
+            )
 
             previous_finished_task_message = f"""The information given to you is as follows.
 There are the specifications to build the application:
 ```
-{self.agents.copilot.storages.docs["specifications.md"]}
+{team.supervisor.storages.docs["specifications.md"]}
 ```
 
 There are the source codes generated so far:
 ```
-{self.agents.copilot.current_source_code()}
+{team.supervisor.current_source_code()}
 ```
 """
             message = Message.create_human_message(
@@ -53,4 +56,3 @@ There are the source codes generated so far:
 
         self.agents.engineer.create_source_code(auto_mode=self.auto_mode)
         self.agents.engineer.complete_source_code(auto_mode=self.auto_mode)
-        self.console.new_lines()
