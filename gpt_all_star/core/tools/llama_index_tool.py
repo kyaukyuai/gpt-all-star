1fb52a367c2da392226cf1b34963f642e423a9b5
kyaukyuai <y.kakui@gmail.com>
2024-02-06 10:53:57 +0900

    Add llama_index_tool to Agent tools

---
+++ gpt_all_star/core/tools/llama_index_tool.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from langchain.agents import Tool
+from llama_index import Document, GPTVectorStoreIndex, SimpleDirectoryReader
+
+from gpt_all_star.core.tools.document_chunker import DocumentChunker
+
+
+def llama_index_tool(path: Path) -> Tool:
+    def name_metadata_store(filename: str) -> dict:
+        return {"filename": filename}
+
+    def update_documents_and_query(query: str) -> str:
+        excluded_file_globs = ["./.archive/**/*", "./node_modules/**/*"]
+        documents = SimpleDirectoryReader(
+            input_dir=str(path.absolute()),
+            recursive=True,
+            exclude=excluded_file_globs,
+            file_metadata=name_metadata_store,
+        ).load_data()
+        print(f"Loaded {len(documents)} documents")
+
+        chunked_langchain_documents = DocumentChunker.chunk_documents(
+            [doc.to_langchain_format() for doc in documents]
+        )
+        chunked_documents = [
+            Document.from_langchain_format(doc) for doc in chunked_langchain_documents
+        ]
+
+        index = GPTVectorStoreIndex.from_documents(documents=chunked_documents)
+
+        return str(index.as_query_engine().query(query))
+
+    return Tool(
+        name="LlamaIndex",
+        func=update_documents_and_query,
+        description="Helpful for querying and retrieving information from existing source code.",
+        return_direct=False,
+    )
