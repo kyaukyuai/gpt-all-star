1941e4bbfac6669d9b64739280378779da9e4f82
kyaukyuai <y.kakui@gmail.com>
2024-02-12 22:59:42 +0900

    refactor: 💡 fix handle_error function :bug:

--- gpt_all_star/core/agents/project_manager/project_manager.py
+++ gpt_all_star/core/agents/project_manager/project_manager.py
@@ -1,12 +1,7 @@
 from __future__ import annotations
 
-from gpt_all_star.core.message import Message
 from gpt_all_star.core.storage import Storages
-from gpt_all_star.core.agents.agent import Agent, AgentRole, NEXT_COMMAND
-from gpt_all_star.core.agents.project_manager.planning_development_prompt import (
-    planning_development_template,
-)
-from gpt_all_star.helper.text_parser import TextParser
+from gpt_all_star.core.agents.agent import Agent, AgentRole
 
 
 class ProjectManager(Agent):
@@ -18,65 +13,3 @@ class ProjectManager(Agent):
         profile: str | None = None,
     ) -> None:
         super().__init__(AgentRole.PROJECT_MANAGER, storages, debug_mode, name, profile)
-
-    def plan_development(self, review_mode: bool = False):
-        self.messages.append(
-            Message.create_system_message(
-                planning_development_template.format(
-                    specifications=self.storages.docs["specifications.md"],
-                    technologies=self.storages.docs["technologies.md"],
-                    pages=self.storages.docs["pages.md"],
-                    files=self.storages.docs["files.md"],
-                    json_format="""
-{
-    "plan": {
-        "type": "array",
-        "description": "List of development tasks that need to be done to implement the entire plan.",
-        "items": {
-            "type": "object",
-            "description": "Development task that needs to be done to implement the entire plan. It contains all details that developer who is not familiar with project needs to know to implement the task.",
-            "properties": {
-                "todo": {
-                    "type": "string",
-                    "description": "Very detailed description of the actual TODO to be performed to accomplish the entire plan.",
-                },
-                "goal": {
-                    "type": "string",
-                    "description": "Very detailed description of the goals to be achieved for the TODO to be executed to accomplish the entire plan",
-                }
-            },
-            "required": ["todo", "goal"],
-        },
-    }
-}
-""",
-                    example="""
-------------------------example_1---------------------------
-```
-{
-    "plan": [
-        {
-            "todo": "",
-            "goal": ""
-        },
-        {
-            "todo": "",
-            "goal": ""
-        }
-    ]
-}
-```
-------------------------example_1---------------------------
-""",
-                ),
-            )
-        )
-
-        self.execute(
-            "Do you want to add any features or changes? If yes, describe it here and if no, just type `{}`".format(
-                NEXT_COMMAND
-            ),
-            review_mode=review_mode,
-        )
-
-        return TextParser.to_json(self.latest_message_content())
--- gpt_all_star/core/agents/qa_engineer/qa_engineer.py
+++ gpt_all_star/core/agents/qa_engineer/qa_engineer.py
@@ -6,16 +6,8 @@ import threading
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-from gpt_all_star.core.message import Message
 from gpt_all_star.core.storage import Storages
 from gpt_all_star.core.agents.agent import Agent, AgentRole
-from gpt_all_star.core.agents.qa_engineer.planning_healing_prompt import (
-    planning_healing_template,
-)
-from gpt_all_star.core.agents.qa_engineer.implement_planning_prompt import (
-    implement_planning_template,
-)
-from gpt_all_star.helper.text_parser import TextParser
 
 
 class QAEngineer(Agent):
@@ -28,12 +20,7 @@ class QAEngineer(Agent):
     ) -> None:
         super().__init__(AgentRole.QA_ENGINEER, storages, debug_mode, name, profile)
 
-    def execute_code(self, review_mode: bool = False) -> None:
-        command = self.storages.root["run.sh"]
-        self._confirm_execution(review_mode, command)
-        self._run_command()
-
-    def _confirm_execution(self, review_mode: bool, command: str) -> None:
+    def confirm_execution(self, review_mode: bool, command: str) -> None:
         if review_mode:
             self.console.new_lines()
             CONFIRM_CHOICES = ["yes", "no"]
@@ -61,58 +48,54 @@ class QAEngineer(Agent):
         )
         self.console.new_lines()
 
-    def _run_command(self) -> None:
+    def run_command(self) -> None:
         command = "bash run.sh"
-        MAX_ATTEMPTS = 5
-        for attempt in range(MAX_ATTEMPTS):
-            self.state(f"Attempt {attempt + 1}/{MAX_ATTEMPTS}")
-            try:
-                process = subprocess.Popen(
-                    command,
-                    shell=True,
-                    cwd=self.storages.root.path,
-                    text=True,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
+        try:
+            process = subprocess.Popen(
+                command,
+                shell=True,
+                cwd=self.storages.root.path,
+                text=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+
+            stdout_lines = []
+            stderr_lines = []
+
+            def read_stdout():
+                for line in iter(process.stdout.readline, ""):
+                    stdout_lines.append(line.strip())
+                    self.console.print(f"{line.strip()}", style="green")
+
+            def read_stderr():
+                for line in iter(process.stderr.readline, ""):
+                    stderr_lines.append(line.strip())
+                    self.console.print(f"{line.strip()}", style="red")
+
+            stdout_thread = threading.Thread(target=read_stdout)
+            stderr_thread = threading.Thread(target=read_stderr)
+            stdout_thread.start()
+            stderr_thread.start()
+
+            if self._wait_for_server():
+                self._check_browser_errors()
+                return
+
+            stdout_thread.join()
+            stderr_thread.join()
+
+            return_code = process.wait()
+            if return_code != 0:
+                raise Exception(
+                    {
+                        "stdout": "\n".join(stdout_lines),
+                        "stderr": "\n".join(stderr_lines),
+                    }
                 )
-
-                stdout_lines = []
-                stderr_lines = []
-
-                def read_stdout():
-                    for line in iter(process.stdout.readline, ""):
-                        stdout_lines.append(line.strip())
-                        self.console.print(f"{line.strip()}", style="green")
-
-                def read_stderr():
-                    for line in iter(process.stderr.readline, ""):
-                        stderr_lines.append(line.strip())
-                        self.console.print(f"{line.strip()}", style="red")
-
-                stdout_thread = threading.Thread(target=read_stdout)
-                stderr_thread = threading.Thread(target=read_stderr)
-                stdout_thread.start()
-                stderr_thread.start()
-
-                if self._wait_for_server():
-                    self._check_browser_errors()
-                    return
-
-                stdout_thread.join()
-                stderr_thread.join()
-
-                return_code = process.wait()
-                if return_code != 0:
-                    self._handle_error(
-                        {
-                            "stdout": "\n".join(stdout_lines),
-                            "stderr": "\n".join(stderr_lines),
-                        }
-                    )
-                process.terminate()
-            except KeyboardInterrupt:
-                self._handle_keyboard_interrupt()
-                break
+            process.terminate()
+        except KeyboardInterrupt:
+            self._handle_keyboard_interrupt()
 
     def _wait_for_server(self) -> bool:
         MAX_ATTEMPTS = 30
@@ -141,99 +124,7 @@ class QAEngineer(Agent):
                 errors += f"{entry['message']}\n"
         driver.quit()
         if errors:
-            self._handle_error({"browser errors": errors})
-
-    def _handle_error(self, e: dict) -> None:
-        self.state("Initiating source code correction process.")
-
-        self.messages.append(
-            Message.create_system_message(
-                planning_healing_template.format(
-                    errors=e,
-                    codes=self.current_source_code(),
-                    json_format="""
-{
-    "plan": {
-        "type": "array",
-        "description": "List of tasks to fix the errors.",
-        "items": {
-            "type": "object",
-            "description": "Task to fix the errors.",
-            "properties": {
-                "todo": {
-                    "type": "string",
-                    "description": "Very detailed description of the actual TODO to be performed to accomplish the entire plan.",
-                },
-                "goal": {
-                    "type": "string",
-                    "description": "Very detailed description of the goals to be achieved for the TODO to be executed to accomplish the entire plan",
-                }
-            },
-            "required": ["todo", "goal"],
-        },
-    }
-}
-""",
-                    example="""
-------------------------example_1---------------------------
-```
-{
-    "plan": [
-        {
-            "todo": "",
-            "goal": "",
-        },
-        {
-            "todo": "",
-            "goal": "",
-        }
-    ]
-}
-```
-------------------------example_1---------------------------
-""",
-                )
-            )
-        )
-        self.invoke()
-        self.console.new_lines(2)
-
-        todo_list = TextParser.to_json(self.latest_message_content())
-
-        for i, task in enumerate(todo_list["plan"]):
-            self.state(
-                f"""
-TODO {i + 1}: {task['todo']}")
-GOAL: {task['goal']}
----
-"""
-            )
-
-            previous_finished_task_message = f"""The information given to you is as follows.
-There are the specifications to build the application:
-```
-{self.storages.docs["specifications.md"]}
-```
-
-There are the source codes generated so far:
-```
-{self.current_source_code()}
-```
-"""
-            self.messages.append(
-                Message.create_system_message(
-                    implement_planning_template.format(
-                        todo_description=task["todo"],
-                        finished_todo_message=previous_finished_task_message,
-                        todo_goal=task["goal"],
-                    )
-                )
-            )
-            self.invoke()
-            self.console.new_lines(2)
-            files = TextParser.parse_code_from_text(self.latest_message_content())
-            for file_name, file_content in files:
-                self.storages.root[file_name] = file_content
+            raise Exception({"browser errors": errors})
 
     def _handle_keyboard_interrupt(self) -> None:
         self.console.new_lines()
