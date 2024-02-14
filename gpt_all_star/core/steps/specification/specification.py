8e6502a7bdc35654b254092fd2a5ec514494c270
kyaukyuai <y.kakui@gmail.com>
2024-02-15 08:52:38 +0900

    Add additional tasks to team planning

---
+++ gpt_all_star/core/steps/specification/specification.py
@@ -0,0 +1,32 @@
+from gpt_all_star.core.agents import agents
+from gpt_all_star.core.steps.specification.additional_tasks import (
+    create_additional_tasks,
+)
+from gpt_all_star.core.steps.step import Step
+from gpt_all_star.core.team import Team
+
+
+class Specification(Step):
+    def __init__(
+        self,
+        agents: agents,
+        japanese_mode: bool,
+        review_mode: bool,
+        debug_mode: bool,
+    ) -> None:
+        super().__init__(agents, japanese_mode, review_mode, debug_mode)
+
+    def run(self) -> None:
+        instructions = self.agents.product_owner.get_instructions()
+        app_type = self.agents.product_owner.get_app_type()
+
+        team = Team(
+            supervisor=self.agents.project_manager,
+            members=[
+                self.agents.product_owner,
+                self.agents.designer,
+                self.agents.engineer,
+            ],
+        )
+
+        team.drive(None, create_additional_tasks(app_type, instructions))
