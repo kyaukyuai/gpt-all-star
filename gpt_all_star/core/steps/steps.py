84b01d0992842d70531c63ee1ddbd060ae434902
kyaukyuai <y.kakui@gmail.com>
2024-02-14 22:49:34 +0900

    Refactor improvement step to use new implementation

--- gpt_all_star/core/steps/steps.py
+++ gpt_all_star/core/steps/steps.py
@@ -3,7 +3,7 @@ from enum import Enum
 from gpt_all_star.core.steps.deployment import Deployment
 from gpt_all_star.core.steps.development.development import Development
 from gpt_all_star.core.steps.execution import Execution
-from gpt_all_star.core.steps.improvement import Improvement
+from gpt_all_star.core.steps.improvement.improvement import Improvement
 from gpt_all_star.core.steps.specification import Specification
 from gpt_all_star.core.steps.system_design import SystemDesign
 from gpt_all_star.core.steps.team_building import TeamBuilding
