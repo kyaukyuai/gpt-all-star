8a8493b0aeeb2d8f4c93f964c47d8138dfeb90a4
kyaukyuai <y.kakui@gmail.com>
2024-02-24 22:35:08 +0900

    Update healing logic in execution module

--- gpt_all_star/core/execution/execution.py
+++ gpt_all_star/core/execution/execution.py
@@ -1,7 +1,7 @@
 from gpt_all_star.core.agents.chain import Chain
 from gpt_all_star.core.agents.copilot import Copilot
-from gpt_all_star.core.execution.planning_prompt import planning_prompt_template
 from gpt_all_star.core.message import Message
+from gpt_all_star.core.steps.healing.healing import Healing
 from gpt_all_star.core.team import Team
 
 
@@ -51,10 +51,5 @@ Generate an command to execute the application.
             except KeyboardInterrupt:
                 break
             except Exception as e:
-                planning_prompt = planning_prompt_template.format(
-                    error=e,
-                    current_source_code=self.copilot.storages.current_source_code(),
-                )
-                for agent in self.team.agents.to_array():
-                    agent.set_executor(self.working_directory)
-                self.team._run(planning_prompt)
+                healing = Healing(copilot=self.copilot, error_message=e)
+                self.team.run(healing)
