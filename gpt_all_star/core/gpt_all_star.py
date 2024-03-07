968065f3ca1d2c5891ee5a72009f2a53d41a4312
kyaukyuai <y.kakui@gmail.com>
2024-03-07 09:42:25 +0900

    Add GptAllStar chat method implementation

---
+++ gpt_all_star/core/gpt_all_star.py
@@ -0,0 +1,11 @@
+from gpt_all_star.core.project import Project
+from gpt_all_star.core.steps.steps import StepType
+
+
+class GptAllStar:
+    def __init__(self):
+        pass
+
+    def chat(self, project_name: str, step: StepType = None, message=None):
+        project = Project(step=step, project_name=project_name)
+        project.chat(message=message)
