d4dc65641f350e092ba5ecc310b75f52ceb35290
mskotk <otake@tenfrontier.com>
2024-04-01 00:37:16 +0900

    add improve function in mock_ui_design.py

--- gpt_all_star/core/steps/mock_ui_design/mock_ui_design.py
+++ gpt_all_star/core/steps/mock_ui_design/mock_ui_design.py
@@ -1,5 +1,8 @@
 from gpt_all_star.core.agents.copilot import Copilot
 from gpt_all_star.core.steps.mock_ui_design.additional_tasks import additional_tasks
+from gpt_all_star.core.steps.mock_ui_design.improvement_prompt import (
+    improvement_prompt_template,
+)
 from gpt_all_star.core.steps.step import Step
 
 
@@ -23,3 +26,13 @@ class MockUiDesign(Step):
             self.copilot.output_files(design_html)
 
         return has_design
+
+    def improvement_prompt(self) -> str:
+        request = self.improvement_request or self.copilot.ask(
+            self._("What do you want to update?"), is_required=True, default=None
+        )
+        improvement_prompt = improvement_prompt_template.format(
+            request=request,
+            mock_ui_design=self.copilot.storages.docs.get("design_000.html", "N/A"),
+        )
+        return improvement_prompt
